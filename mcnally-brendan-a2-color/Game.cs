aa1b05430c256a7aac3a06fbc42ff601779f6baf
Brendan McNally <brendanmcnally295@gmail.com>
2025-01-27 11:14:18 -0500

    Shapeshifter!

--- mcnally-brendan-a2-color/Game.cs
+++ mcnally-brendan-a2-color/Game.cs
@@ -1,29 +1,35 @@
-﻿// Include the namespaces (code libraries) you need below.
+﻿
 using System;
 using System.Numerics;
-//testing this github
-// The namespace your code is in.
+//  _____ _    _          _____  ______    _____ _    _ _____ ______ _______ ______ _____  
+//  / ____| |  | |   /\   |  __ \|  ____|  / ____| |  | |_   _|  ____|__   __|  ____|  __ \ 
+// | (___ | |__| |  /  \  | |__) | |__    | (___ | |__| | | | | |__     | |  | |__  | |__) |
+//  \___ \|  __  | / /\ \ |  ___/|  __|    \___ \|  __  | | | |  __|    | |  |  __| |  _  / 
+//  ____) | |  | |/ ____ \| |    | |____   ____) | |  | |_| |_| |       | |  | |____| | \ \ 
+// |_____/|_|  |_/_/    \_\_|    |______| |_____/|_|  |_|_____|_|       |_|  |______|_|  \_\
+                                                                                          
+              
+// WELCOME TO SHAPE SHIFTER, MY INTERACTIVE DRAWING GAME 
+// I WILL MAKE THIS CODE SUPER READABLE :) :) :) :): - )
+
+
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
+
     public class Game
     {
-        // Place your variables here:
+        // Place our variables here:
 
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
         public void Setup()
         {
+            //Setting up the Visuals    
+            
 
         }
 
-        /// <summary>
+        
         ///     Update runs every frame.
-        /// </summary>
+        
         public void Update()
         {
 
